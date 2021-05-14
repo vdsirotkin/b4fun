bb8d8bdad388fd7352f388fa5ef27d25cb81e283
Vitaly Sirotkin <vdsirotkin@dev.vtb>
2021-05-14 12:32:46 +0300

    fix

--- src/middlewares/attachChat.ts
+++ src/middlewares/attachChat.ts
@@ -2,7 +2,9 @@ import {Context} from "telegraf";
 import {getChatInfo} from "@/models";
 
 export async function attachChat(ctx: Context, next) {
-    console.log('chat id == ' + ctx.chat.id)
-    ctx.chatInfo = await getChatInfo(ctx.chat.id)
+    if (ctx.chat) {
+        console.log('chat id == ' + ctx.chat.id);
+        ctx.chatInfo = await getChatInfo(ctx.chat.id)
+    }
     return next()
 }
